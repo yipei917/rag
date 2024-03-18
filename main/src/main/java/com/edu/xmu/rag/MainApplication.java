f1ef5816a4fc5c2ec1d7cfb77819162d6bf4e6ba
Weeknd <1351049203@qq.com>
2024-03-18 15:11:30 +0800

    main

--- main/src/main/java/com/edu/xmu/rag/MainApplication.java
+++ main/src/main/java/com/edu/xmu/rag/MainApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.edu.xmu.rag.core",
+        "com.edu.xmu.rag"})
 @EnableJpaRepositories(value = "com.edu.xmu.rag.core.jpa", repositoryBaseClass = SelectiveUpdateJpaRepositoryImpl.class, basePackages = "com.edu.xmu.rag.mapper")
 public class MainApplication {
 
